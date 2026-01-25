91f54a5031cc2604eabb8d4503187ea30d57eccf
mainproject2025 <mainproject033@gmail.com>
2026-01-25 12:16:31 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(
-      require("../../serviceAccountKey.json")
+      require("/etc/secrets/serviceAccountKey.json")
     ),
   });
 }
